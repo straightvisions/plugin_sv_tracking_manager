70720742b6227976eb0d815b04b928ac6115ebe2
Matthias Reuter <matthias-reuter@straightvisions.com>
2018-09-26 15:51:46 +0200

    Tracking: Support von Custom Events

--- lib/modules/custom_events.php
+++ lib/modules/custom_events.php
@@ -16,7 +16,13 @@ class custom_events extends modules{
 		static::$s['custom_events']->set_title(__('Custom Events', $this->get_module_name()));
 		static::$s['custom_events']->load_type('group');
 		static::$s['custom_events']->set_loop(true);
-		//static::$settings['custom_events']->set_callback(array($this,'array'));
+		
+		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child->set_ID('event');
+		$child->set_title(__('Event Trigger', $this->get_module_name()));
+		$child->set_description(__('Selected trigger will be monitored for event action, see https://www.w3schools.com/jquery/jquery_events.asp', $this->get_module_name()));
+		$child->load_type('text');
+		$child->set_placeholder('click');
 
 		$child												= static::$s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('element');
@@ -47,6 +53,12 @@ class custom_events extends modules{
 		$child->set_title(__('eventValue', $this->get_module_name()));
 		$child->set_description(__('A numeric value associated with the event (e.g. 42)', $this->get_module_name()));
 		$child->load_type('number');
+		
+		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child->set_ID('active_page');
+		$child->set_title(__('Active Page', $this->get_module_name()));
+		$child->set_description(__('Optional, if you do not want to apply this event globally on site, but on a specific page.', $this->get_module_name()));
+		$child->load_type('select_page');
 	}
 	public function init(){
 		add_action('admin_init', array($this, 'admin_init'));
@@ -60,7 +72,31 @@ class custom_events extends modules{
 		
 		if(!is_admin()){
 			$this->load_settings();
+			add_action('wp_head',array($this,'wp_head'), 1000);
 		}
+	}
+	public function wp_head()
+	{
+		echo '<script data-id="' . $this->get_name() . '">';
+		$events = static::$s['custom_events']->run_type()->get_data();
+		if ($events && is_array($events) && count($events) > 0) {
+			foreach ($events as $event) {
+				if(strlen($event['event']) == 0){
+					continue;
+				}
+				if (isset($event['active_page']) && intval($event['active_page']) > 0 && intval($event['active_page']) != get_queried_object_id()) {
+					continue;
+				}
+				echo '
+				jQuery(document).on("'.$event['event'].'", "'.$event['element'].'", function(){
+					console.log("'.addslashes($event['element'].' / '.$event['event'].' triggered: eventAction: '.$event['eventAction'].' eventLabel: '.$event['eventLabel'].' eventValue: '.$event['eventValue']).'");
+					ga("send", "event", "'.$event['eventCategory'].'", "'.$event['eventAction'].'", "'.$event['eventLabel'].'", '.((intval($event['eventValue']) > 0) ? intval($event['eventValue']) : 0).');
+				});
+				';
+			}
+		}
+		//	     // Send data using an event.
+		echo '</script>';
 	}
 	public function menu(){
 		add_menu_page(
