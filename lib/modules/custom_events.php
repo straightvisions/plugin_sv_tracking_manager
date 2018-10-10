1409d0d42ebdb1a5c4ab8f62aad7d1e927b1774a
Matthias Reuter <matthias-reuter@straightvisions.com>
2018-10-10 09:14:28 +0200

    FEAT - ENHANCEMENT - SETTINGS

--- lib/modules/custom_events.php
+++ lib/modules/custom_events.php
@@ -2,59 +2,57 @@
 namespace sv_tracking_manager;
 
 class custom_events extends modules{
-	private static $s					= array();
-	
 	public function __construct(){
 
 	}
 	private function load_settings(){
 		// Uploaded Fonts
-		static::$s['custom_events']					= static::$settings->create($this);
-		static::$s['custom_events']->set_section_name(__('Custom Events',$this->get_module_name()));
-		static::$s['custom_events']->set_section_description('Set Custom Analytics Elements');
-		static::$s['custom_events']->set_ID('custom_events');
-		static::$s['custom_events']->set_title(__('Custom Events', $this->get_module_name()));
-		static::$s['custom_events']->load_type('group');
-		static::$s['custom_events']->set_loop(true);
+		$this->s['custom_events']					= static::$settings->create($this);
+		$this->s['custom_events']->set_section_name(__('Custom Events',$this->get_module_name()));
+		$this->s['custom_events']->set_section_description('Set Custom Analytics Elements');
+		$this->s['custom_events']->set_ID('custom_events');
+		$this->s['custom_events']->set_title(__('Custom Events', $this->get_module_name()));
+		$this->s['custom_events']->load_type('group');
+		$this->s['custom_events']->set_loop(true);
 		
-		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child												= $this->s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('event');
 		$child->set_title(__('Event Trigger', $this->get_module_name()));
 		$child->set_description(__('Selected trigger will be monitored for event action, see https://www.w3schools.com/jquery/jquery_events.asp', $this->get_module_name()));
 		$child->load_type('text');
 		$child->set_placeholder('click');
 
-		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child												= $this->s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('element');
 		$child->set_title(__('DOM Element', $this->get_module_name()));
 		$child->set_description(__('DOM Selector (e.g. .contact_form, #submit)', $this->get_module_name()));
 		$child->load_type('text');
 
-		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child												= $this->s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('eventCategory');
 		$child->set_title(__('eventCategory', $this->get_module_name()));
 		$child->set_description(__('Typically the object that was interacted with (e.g. "Video")', $this->get_module_name()));
 		$child->load_type('text');
 
-		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child												= $this->s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('eventAction');
 		$child->set_title(__('eventAction', $this->get_module_name()));
 		$child->set_description(__('The type of interaction (e.g. "play")', $this->get_module_name()));
 		$child->load_type('text');
 
-		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child												= $this->s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('eventLabel');
 		$child->set_title(__('eventLabel', $this->get_module_name()));
 		$child->set_description(__('Useful for categorizing events (e.g. "Fall Campaign")', $this->get_module_name()));
 		$child->load_type('text');
 
-		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child												= $this->s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('eventValue');
 		$child->set_title(__('eventValue', $this->get_module_name()));
 		$child->set_description(__('A numeric value associated with the event (e.g. 42)', $this->get_module_name()));
 		$child->load_type('number');
 		
-		$child												= static::$s['custom_events']->run_type()->add_child($this);
+		$child												= $this->s['custom_events']->run_type()->add_child($this);
 		$child->set_ID('active_page');
 		$child->set_title(__('Active Page', $this->get_module_name()));
 		$child->set_description(__('Optional, if you do not want to apply this event globally on site, but on a specific page.', $this->get_module_name()));
@@ -78,7 +76,7 @@ class custom_events extends modules{
 	public function wp_head()
 	{
 		echo '<script data-id="' . $this->get_name() . '">';
-		$events = static::$s['custom_events']->run_type()->get_data();
+		$events = $this->s['custom_events']->run_type()->get_data();
 		if ($events && is_array($events) && count($events) > 0) {
 			foreach ($events as $event) {
 				if(strlen($event['event']) == 0){
