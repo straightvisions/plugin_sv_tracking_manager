0ecf412663a601ea8a5818746b6adb893fd4f3c9
Matthias Reuter <matthias-reuter@straightvisions.com>
2018-10-17 10:26:40 +0200

    FEAT - REFACTORING - SETTINGS

--- lib/backend/tpl/custom_events.php
+++ lib/backend/tpl/custom_events.php
@@ -4,16 +4,13 @@
 	<div id="section_<?php echo $section_name; ?>" class="sv_content_wrapper">
 		<div class="sv_content">
 		<div class="sv_settings">
-			<!--<div class="sv_side_menu">
-				<img src="<?php echo $this->get_url_lib_core('assets/logo.png'); ?>" />
-			</div>-->
 			<div class="bypass_sv_content_wrapper">
 				<div class="sv_content_title">
 					<h1><?php echo get_admin_page_title(); ?></h1>
 				</div>
 				<div class="sv_content">
 					<?php
-						echo static::$settings->get_module_settings_form($this->modules->custom_events);
+						echo static::$settings->get_module_settings_form($section['object']);
 					?>
 				</div>
 			</div>
