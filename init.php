b31408f5fad3de9a397ad13b8733619fb877ff99
Matthias Bathke <info@straightvisions.com>
2020-02-12 12:18:41 +0100

    SUB - UPDATE - CORE FEAT - ADD - ANONYMIZE IP FOR GOOGLE ANALYTICS

--- init.php
+++ init.php
@@ -6,8 +6,8 @@
 	}
 	
 	class init extends \sv_core\core_plugin {
-		const version = 1401;
-		const version_core_match = 4020;
+		const version = 1402;
+		const version_core_match = 4022;
 		
 		public function load(){
 			if(!$this->setup( __NAMESPACE__, __FILE__ )){
